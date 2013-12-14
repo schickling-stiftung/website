3c0fdba9b9d78023edc0b93d121bd5d9178acfe0
Johannes Schickling <schickling.j@gmail.com>
2013-12-14 21:28:38 +0100

    Fixed path

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ mkdir output
 cd output
 git init
 git checkout -b gh-pages
-git remote add origin git@github.com:schickling-stiftung/website.git
+git remote add origin git@github.com:schickling-stiftung/schickling-stiftung.github.io.git
 git pull origin gh-pages
 git rm -r *
 cd ..
