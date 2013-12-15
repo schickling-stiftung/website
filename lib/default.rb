0a2aeeebeec7662c2a30ab7e2c532201419ac25f
Johannes Schickling <schickling.j@gmail.com>
2013-12-15 11:28:29 +0100

    Header + fallback

--- lib/default.rb
+++ lib/default.rb
@@ -1,2 +1,21 @@
 # All files in the 'lib' directory will be loaded
 # before nanoc starts compiling.
+
+def header_path (item)
+    path = item.path
+    until path == '/' do
+        full_path = "#{File.dirname(__FILE__)}/../content#{path}"
+        if File.exist?(full_path + 'header.jpg')
+            puts path
+            break
+        else
+            path_tokens = path.split('/')
+            if path_tokens.size > 1
+                path = (path_tokens.first path_tokens.size - 1).join('/')
+            else
+                path = '/'
+            end
+        end
+    end
+    path + 'header.jpg'
+end
