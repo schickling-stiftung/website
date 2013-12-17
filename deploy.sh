55873fbb62feb578e69353c8ede271b6c4e18257
Johannes Schickling <schickling.j@gmail.com>
2013-12-17 13:17:31 +0100

    Fixed deploy file

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,12 @@ rm -rf output
 mkdir output
 cd output
 git init
-git checkout -b gh-pages
 git remote add origin git@github.com:schickling-stiftung/schickling-stiftung.github.io.git
-git pull origin gh-pages
+git pull origin master
 git rm -r *
 cd ..
 nanoc compile
 cd output
 git add .
 git commit -am "Deploy"
-git push origin gh-pages
+git push origin master
